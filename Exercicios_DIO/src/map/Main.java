e0f8635c06603dbf0787cbe6bba08ef8704aa8e6
Warlley Dias <wmedeiros.d@gmail.com>
2026-05-08 12:00:44 -0300

    adicionado o uso do Map

--- Exercicios_DIO/src/map/Main.java
+++ Exercicios_DIO/src/map/Main.java
@@ -1,16 +1,60 @@
 package map;
 
-import java.util.HashMap;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class Main {
 
 	public static void main(String[] args) {
-		Map<String, User> users = new HashMap<>();
+		
+		Map<String, User> users = new TreeMap<>();
 		users.put ("joao@joao.com", new User("Joao", 22));
 		users.put ("maria@maria.com", new User("Maira", 32));
 		users.put ("juca@juca.com", new User("Juca", 18));
 		users.put ("leo@leo.com", new User("Leo", 40));
+		
+		System.out.println(users);
+		
+		
+		
+		
+		
+		/*Map<String, User> users = new HashMap<>();
+		users.put ("joao@joao.com", new User("Joao", 22));
+		users.put ("maria@maria.com", new User("Maira", 32));
+		users.put ("juca@juca.com", new User("Juca", 18));
+		users.put ("leo@leo.com", new User("Leo", 40));
+		
+		System.out.println(users);
+		
+		
+		System.out.println("------------------");
+
+		
+		
+		users.putIfAbsent("", new User("", -1));
+		System.out.println(users);
+		
+		
+		users.replace("joao@joao.com", new User("Joao", 33));
+		
+		users.forEach((k, v) -> System.out.printf("key: %s | value: %s \n", k, v));
+		
+		System.out.println("------------------");
+		
+		System.out.println(users.isEmpty());
+		
+		System.out.println("------------------");
+		
+		users.merge("joao@joao.com", new User(" ", -1), (user, user2) ->{
+			
+			System.out.println(user);
+			System.out.println(user2);
+			
+			return user2;
+			
+		});
+		
 		
 		System.out.println(users);
 		
@@ -34,7 +78,7 @@ public class Main {
 		
 		System.out.println("===================");
 		
-		System.out.println(users.remove("joao@joao.com",new User("Joao", 22) ));
+		System.out.println(users.remove("joao@joao.com",new User("Joao", 22) ));*/
 		
 	}
 
--- Exercicios_DIO/src/map/User.java
+++ Exercicios_DIO/src/map/User.java
@@ -1,5 +1,11 @@
 package map;
 
-public record User(String name, int age) {
+public record User(String name, int age) implements Comparable<User>{
 
+	@Override
+	public int compareTo(final User outro) {
+		
+			return this.age - outro.age;
+	}
+	
 }
